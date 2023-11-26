04ffd4c6e8b0353bdd6136109680e7be1865549e
dixnne <dianapa.narvaez@gmail.com>
2023-11-26 17:18:14 -0600

    add php index with include for header and footer

--- header/header-login.php
+++ header/header-login.php
@@ -1,18 +1,3 @@
-<!DOCTYPE html>
-<html>
-<head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
-        <link rel="stylesheet" href="../css/styles.css">
-    </head>
-    <body id="bootstrap-override" class="bg-color">
         <section id="header" class="bg-dark">
             <header class="p-3">
                 <div class="container">
@@ -125,7 +110,3 @@
                 </header>
             </div>
         </section></div>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-        <script src="" async defer></script>
-    </body>
-</html>

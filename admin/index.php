4662e318fe02431fa9714c83b01626c2143d746f
dixnne <dianapa.narvaez@gmail.com>
2023-11-30 17:56:23 -0600

    Adminside menu and sessions functionality

--- admin/index.php
+++ admin/index.php
@@ -1,36 +1,85 @@
 <?php
-    session_start();
-    if (isset($_SESSION["username"]) && isAdmin($_SESSION["username"])) {
-        ?>
+session_start();
+?>
 <!DOCTYPE html>
 <html>
     <head>
         <meta charset="utf-8">
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title></title>
+        <title>Gift uwu Store</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
         <link href="https://fonts.googleapis.com/css2?family=Bungee&display=swap" rel="stylesheet">
+        <link href="https://cdn.jsdelivr.net/npm/sweetalert2@11.10.0/dist/sweetalert2.min.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
         <link rel="stylesheet" href="../css/styles.css">
+        <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
     </head>
     <body id="bootstrap-override" class="bg-color">
         <?php
-            require("../header/header-admin.html");
-        ?>
-        <div class="">
+        include("../header/header-admin-login.html");
 
-        </div>
-        </div></div></div></section>
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            require("../database/db-setup.php");
+            require("../database/db-handle.php");
+
+            $username = $password = "";
+            $username = test_input($_POST["username"]);
+            $password = test_input($_POST["password"]);
+        }
+
+        if (isset($_POST["submit"])){
+            if (validateUser($username, $password) && isAdmin($username)) {
+                $_SESSION["username"] = $username;
+                $row = getUser($username);
+                $_SESSION["image"] = $row["image"];
+                echo '<script>
+                swal("Bienvenido a Gift uwu Store!", "Sesión iniciada con éxito", "success").then(function() {
+                    window.location = "./home.php";
+                });
+                </script>';
+            } else {
+                echo '<script>
+                swal("Datos Incorrectos", "Inténtalo nuevamente...", "warning");
+                </script>';
+            }
+        }
+        ?>
+        <section id="form">
+            <h1 class="text-center font-paytone mt-5">Identifícate!</h1>
+            <div class="container col-12 col-md-6 p-5 bg-color2 mb-5 rounded-5 shadow">
+                <form action="<?php echo $_SERVER['PHP_SELF'];?>" method="post" class="row needs-validation" enctype="multipart/form-data" novalidate>
+                    <div class="col-12 col-md-6 p-2">
+                        <label for="inputUser" class="form-label">Nombre de Usuario</label>
+                        <input type="text" name="username" value="<?php echo isset($_POST['username']) ? $_POST['username'] : '' ?>" id="inputUser" class="form-control" aria-describedby="userHelpBlock" required>
+                        <div id="userHelpBlock" class="form-text">
+                            Ingresa el nombre de usuario de tu cuenta.
+                        </div>
+                        <div class="invalid-feedback">
+                            Tenemos que identificarte con algo...
+                        </div>
+                    </div>
+                    <div class="col-12 col-md-6 p-2">
+                        <label for="inputPassword" class="form-label">Contraseña</label>
+                        <input type="password" name="password" value="<?php echo isset($_POST['password']) ? $_POST['password'] : '' ?>" id="inputPassword" class="form-control" aria-describedby="passwordHelpBlock" required>
+                        <div id="passwordHelpBlock" class="form-text">
+                            Espero que no la hayas olvidado!
+                        </div>
+                        <div class="invalid-feedback">
+                            Tienes que ingresar una contraseña.
+                        </div>
+                    </div>
+                    <div class="col-12 text-center">
+                        <button type="submit" name="submit" class="btn btn-dark w-100">Iniciar sesión</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
-        <script src="" async defer></script>
+        <script src="./validation.js" async defer></script>
     </body>
 </html>
-<?php
-    } else {
-        # code...
-    }
-    
-?>
---
+++ header/header-admin-login.php
@@ -0,0 +1,83 @@
+<section id="header" class="bg-dark">
+    <header class="p-3">
+        <div class="container">
+            <div class="row d-flex align-items-center justify-content-center justify-content-lg-between">
+                <div class="d-flex col-12 col-md-3 justify-content-center align-items-center justify-content-lg-start mb-2 mb-lg-0">
+                    <span class="h6 text-white">Admin</span>
+                </div>
+                <a href="#" class="d-flex justify-content-center align-items-center mb-2 mb-lg-0 link-body-emphasis text-decoration-none col-12 col-md-6">
+                    <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-gift-fill color3 mx-3" viewBox="0 0 16 16">
+                        <path d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A2.968 2.968 0 0 1 3 2.506zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43a.522.522 0 0 0 .023.07M9 3h2.932a.56.56 0 0 0 .023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0zm6 4v7.5a1.5 1.5 0 0 1-1.5 1.5H9V7zM2.5 16A1.5 1.5 0 0 1 1 14.5V7h6v9z"/>
+                    </svg>
+                    <span class="h1 text-white font-paytone mt-2">Gift uwu Store</span>
+                </a>
+                <div class="dropdown text-end col-md-3">
+                    <a href="#" class="d-block link-body-emphasis text-decoration-none dropdown-toggle text-white" data-bs-toggle="dropdown" aria-expanded="false">
+                        <img src="<?php echo $_SESSION["image"]; ?>" alt="mdo" width="32" height="32" class="rounded-circle">
+                    </a>
+                    <ul class="dropdown-menu text-small">
+                        <li><a class="dropdown-item" href="#">New project...</a></li>
+                        <li><a class="dropdown-item" href="#">Settings</a></li>
+                        <li><a class="dropdown-item" href="#">Profile</a></li>
+                        <li><hr class="dropdown-divider"></li>
+                        <li><a class="dropdown-item" href="#">Sign out</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </header>
+</section>
+<section>
+    <div class="container-fluid">
+        <div class="row flex-nowrap">
+            <div class="col-auto col-md-2 col-xl-2 px-sm-2 px-0 bg-color4">
+                <div class="d-flex flex-column align-items-center align-items-sm-start p-4 text-white min-vh-100">
+                    <a href="./home" class="d-flex align-items-center pb-3 mb-md-0 me-md-auto text-white text-decoration-none">
+                        <span class="fs-5 d-none d-sm-inline">Menu</span>
+                    </a>
+                    <ul class="nav nav-pills flex-column mb-sm-auto mb-0 align-items-center align-items-sm-start" id="menu">
+                        <li class="nav-item">
+                            <a href="#" class="nav-link align-middle px-0 link-light">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-graph-up-arrow" viewBox="0 0 16 16">
+                                    <path fill-rule="evenodd" d="M0 0h1v15h15v1H0zm10 3.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 .5.5v4a.5.5 0 0 1-1 0V4.9l-3.613 4.417a.5.5 0 0 1-.74.037L7.06 6.767l-3.656 5.027a.5.5 0 0 1-.808-.588l4-5.5a.5.5 0 0 1 .758-.06l2.609 2.61L13.445 4H10.5a.5.5 0 0 1-.5-.5"/>
+                                </svg>
+                                <span class="ms-1 d-none d-sm-inline">Estadísticas</span>
+                            </a>
+                        </li>
+                        <li>
+                            <a href="#submenu1" data-bs-toggle="collapse" class="nav-link link-light px-0 align-middle">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-basket3" viewBox="0 0 16 16">
+                                    <path d="M5.757 1.071a.5.5 0 0 1 .172.686L3.383 6h9.234L10.07 1.757a.5.5 0 1 1 .858-.514L13.783 6H15.5a.5.5 0 0 1 .5.5v1a.5.5 0 0 1-.5.5H.5a.5.5 0 0 1-.5-.5v-1A.5.5 0 0 1 .5 6h1.717L5.07 1.243a.5.5 0 0 1 .686-.172zM3.394 15l-1.48-6h-.97l1.525 6.426a.75.75 0 0 0 .729.574h9.606a.75.75 0 0 0 .73-.574L15.056 9h-.972l-1.479 6h-9.21z"/>
+                                </svg>
+                                <span class="ms-1 d-none d-sm-inline">Productos</span> </a>
+                            <ul class="collapse show nav flex-column ms-1" id="submenu1" data-bs-parent="#menu">
+                                <li class="w-100">
+                                    <a href="#" class="nav-link link-light px-0"> 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-plus-lg" viewBox="0 0 16 16">
+                                        <path fill-rule="evenodd" d="M8 2a.5.5 0 0 1 .5.5v5h5a.5.5 0 0 1 0 1h-5v5a.5.5 0 0 1-1 0v-5h-5a.5.5 0 0 1 0-1h5v-5A.5.5 0 0 1 8 2"/>
+                                        </svg>
+                                        <span class="d-none d-sm-inline">Agregar</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-link link-light px-0"> 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pen" viewBox="0 0 16 16">
+                                        <path d="m13.498.795.149-.149a1.207 1.207 0 1 1 1.707 1.708l-.149.148a1.5 1.5 0 0 1-.059 2.059L4.854 14.854a.5.5 0 0 1-.233.131l-4 1a.5.5 0 0 1-.606-.606l1-4a.5.5 0 0 1 .131-.232l9.642-9.642a.5.5 0 0 0-.642.056L6.854 4.854a.5.5 0 1 1-.708-.708L9.44.854A1.5 1.5 0 0 1 11.5.796a1.5 1.5 0 0 1 1.998-.001m-.644.766a.5.5 0 0 0-.707 0L1.95 11.756l-.764 3.057 3.057-.764L14.44 3.854a.5.5 0 0 0 0-.708l-1.585-1.585z"/>
+                                        </svg>
+                                        <span class="d-none d-sm-inline">Editar</span>
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" class="nav-link link-light px-0"> 
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-trash3" viewBox="0 0 16 16">
+                                        <path d="M6.5 1h3a.5.5 0 0 1 .5.5v1H6v-1a.5.5 0 0 1 .5-.5M11 2.5v-1A1.5 1.5 0 0 0 9.5 0h-3A1.5 1.5 0 0 0 5 1.5v1H2.506a.58.58 0 0 0-.01 0H1.5a.5.5 0 0 0 0 1h.538l.853 10.66A2 2 0 0 0 4.885 16h6.23a2 2 0 0 0 1.994-1.84l.853-10.66h.538a.5.5 0 0 0 0-1h-.995a.59.59 0 0 0-.01 0zm1.958 1-.846 10.58a1 1 0 0 1-.997.92h-6.23a1 1 0 0 1-.997-.92L3.042 3.5zm-7.487 1a.5.5 0 0 1 .528.47l.5 8.5a.5.5 0 0 1-.998.06L5 5.03a.5.5 0 0 1 .47-.53Zm5.058 0a.5.5 0 0 1 .47.53l-.5 8.5a.5.5 0 1 1-.998-.06l.5-8.5a.5.5 0 0 1 .528-.47ZM8 4.5a.5.5 0 0 1 .5.5v8.5a.5.5 0 0 1-1 0V5a.5.5 0 0 1 .5-.5"/>
+                                        </svg>
+                                        <span class="d-none d-sm-inline">Eliminar</span>
+                                    </a>
+                                </li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+            <div class="col">

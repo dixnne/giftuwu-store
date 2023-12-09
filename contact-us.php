e9cc211de3b6e051d9159fde15d855e1f433603b
dixnne <dianapa.narvaez@gmail.com>
2023-12-09 15:56:07 -0600

    Merge footer modifications with date

--- contact-us.php
+++ contact-us.php
@@ -184,7 +184,7 @@
             </div>
         </section>
         <?php
-        include("./footer/footer.html");
+        include("./footer/footer.php");
         ?>
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
--- footer/footer-fixed.php
+++ footer/footer-fixed.php
@@ -16,7 +16,21 @@
     <section class="bg-dark pb-4">
         <div class="container">
             <div class="d-flex flex-wrap justify-content-between align-items-center py-3">
-                <p class="col-md-5 mb-0 text-white">© 2023 Gift uwu Store, Inc. Todos los derechos reservados.</p>
+                <p class="col-md-5 mb-0 text-white"><?php   
+                    // Establecer la zona horaria predeterminada a usar. Disponible desde PHP 5.1
+                    date_default_timezone_set('America/Mexico_City');
+                
+                    // Crear una matriz de meses en español
+                    $meses = array("Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre");
+                
+                    // Obtener el día, mes y año actual
+                    $dia = date('d');
+                    $mes = $meses[date('n')-1];
+                    $ano = date('Y');
+                
+                    // Imprime algo como: 06/Diciembre/2023
+                    echo $dia . '/' . $mes . '/' . $ano;
+                ?>  © Gift uwu Store, Inc. Todos los derechos reservados.</p>
                 <a href="#" class="col-md-2 d-flex align-items-center justify-content-center mb-3 mb-md-0 me-md-auto link-body-emphasis text-decoration-none">
                     <svg xmlns="http://www.w3.org/2000/svg" width="40" height="40" fill="currentColor" class="bi bi-gift-fill color3 mx-3" viewBox="0 0 16 16">
                         <path d="M3 2.5a2.5 2.5 0 0 1 5 0 2.5 2.5 0 0 1 5 0v.006c0 .07 0 .27-.038.494H15a1 1 0 0 1 1 1v1a1 1 0 0 1-1 1H1a1 1 0 0 1-1-1V4a1 1 0 0 1 1-1h2.038A2.968 2.968 0 0 1 3 2.506zm1.068.5H7v-.5a1.5 1.5 0 1 0-3 0c0 .085.002.274.045.43a.522.522 0 0 0 .023.07M9 3h2.932a.56.56 0 0 0 .023-.07c.043-.156.045-.345.045-.43a1.5 1.5 0 0 0-3 0zm6 4v7.5a1.5 1.5 0 0 1-1.5 1.5H9V7zM2.5 16A1.5 1.5 0 0 1 1 14.5V7h6v9z"/>
